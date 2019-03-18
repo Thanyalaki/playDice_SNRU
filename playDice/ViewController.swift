75f7be8e391fead92e48744fae5fadde194c3418
nook <Thayalak.nk59@snru.ac.th>
2019-03-18 15:51:32 +0700

    Initial Commit

---
+++ playDice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  playDice
+//
+//  Created by Student04 on 18/3/2562 BE.
+//  Copyright © 2562 nook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
